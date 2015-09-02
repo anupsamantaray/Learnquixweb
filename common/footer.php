356c4474ec36c59780c8915466b057b0a235023e
carindam23 <arindam23@live.com>
2015-09-02 17:58:20 +0530

    commit

--- common/footer.php
+++ common/footer.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row  footer">
 			<div class="copy text-center">
-				<p class="link"><span>&#169; All rights reserved | Design by&nbsp;<a href="#"> Company Name</a></span></p>
+				<p class="link"><span>&#169; All rights reserved | <a href="#">Learn Quix</a></span></p>
 			</div>
 		</div>
 	</div>
