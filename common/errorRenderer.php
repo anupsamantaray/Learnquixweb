1908e829da25e1231f64ff5764a2297732394c5b
Raju Rana <rana.raju1511@gmail.com>
2015-09-09 02:06:12 +0530

    1st commit

--- common/errorRenderer.php
+++ common/errorRenderer.php
@@ -5,7 +5,7 @@ if( count($errorData)>0) {
 	foreach ($errorData as $error) {
 ?>
 <span class="errorMessage"><img src="images/right_icon.png" />&nbsp;&nbsp;<?PHP echo $error;?>	</span>
-<?
+<?php
 	}
 }
 ?>
@@ -15,9 +15,8 @@ if( count($successData)>0) {
 	foreach ($successData as $success) {
 ?>
 <span id="error-message2">&nbsp;&nbsp;<?PHP echo $success;?>	</span>
-<?
+<?php
 	}
 }
 ?>
-
 </ul>
--- contact_us.php
+++ contact_us.php
@@ -28,7 +28,7 @@
 			$from = 'anup.bapu@gmail.com';
 			$headers  = 'MIME-Version: 1.0' . "\r\n";
 			$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-			$headers .= 'From: LearnQuix<'.$from.'>' . "\r\n";
+			$headers .= 'From: LearnQuix<'.$from.'>'."\r\n";
 
 			@mail( $to, $subject, $body, $headers );
 				
