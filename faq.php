dcc2d962c249fd3f6b29217cdc44afa293b22e50
carindam <carindam23@gmail.com>
2015-10-10 02:46:02 +0530

    commit

--- faq.php
+++ faq.php
@@ -1,123 +1,66 @@
 <?php
 include_once("common/header2.php");
 ?>
-<link href="style.css" rel="stylesheet" type="text/css" />
+<body>
+<div class="main_bg"><!-- start main -->
+	<div class="container">
+		<div class="technology row">
+			<h2>Memory Cards</h2>
+			<div class="col-md-12 rightNew2">
+				<div class="col-md-7 rightNew2">
+					<p class="para">Memory cards are unique way of memorising learning items. Capturing keywords/main formulae on a Card template; the Memory Card is a handy tool while revising Chapters and preparing for exams. It also enables the transfer of subject content to long term memory for better retention by looking at it time and again. We create memory cards for students built in a way that his/her gap in memory is filled with least effort.</p>
+					<div class="memory_option effect5">
+						<h2>;vgkjm;',kj</h2>
+					</div>
+					<!--button class="annibutton">Awesome Button</button-->
+					<div class="read_more">
+						<a href="single-page.html" class="fa-btn btn-1 btn-1e">Memory Cards</a>
+					</div>
+				</div>
+				<div class="col-md-5">
+					<div class="bs-example">
+						<div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">
+							<!-- Carousel indicators -->
+							<ol class="carousel-indicators">
+								<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+								<li data-target="#myCarousel" data-slide-to="1"></li>
+								<li data-target="#myCarousel" data-slide-to="2"></li>
+							</ol>   
+						   <!-- Carousel items -->
+							<div class="carousel-inner">
+								<div class="active item">
+									<img src="images/01.png" alt="" class="">
+								</div>
+								<div class="item">
+									<img src="images/02.png" alt="" class="">
+								</div>
+								<div class="item">
+									<img src="images/03.jpg" alt="" class="">
+								</div>
+							</div>
+							<!-- Carousel nav -->
+							<a class="carousel-control left" href="#myCarousel" data-slide="prev">
+								<span class="fa fa-chevron-left slideleft"></span>
+							</a>
+							<a class="carousel-control right" href="#myCarousel" data-slide="next">
+								<span class="fa fa-chevron-right slideleft"></span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div><!-- end main -->
 
-<link rel="stylesheet" type="text/css" media="screen" href="css/style1.css">
 
-<link rel="stylesheet" type="text/css" href="styles.css" media="all" />
-    <link rel="stylesheet" type="text/css" href="demo.css" media="all" />
-	<!-----pop up------>
-<script type="text/javascript" src="js/jquery-1.6.min.js"></script>
-<script type="text/javascript" src="js/jquery.reveal.js"></script>
-<link rel="stylesheet" href="css/reveal.css">
-<!-----pop up ned------>
-	
-    <!-- jQuery -->
-   	<script type="text/javascript" src="scripts/jquery-1.9.0.min.js"></script>
-    <!-- FlexSlider -->
-    <script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
-    <script type="text/javascript" charset="utf-8">
-    var $ = jQuery.noConflict();
-    $(window).load(function() {
-    $('.flexslider').flexslider({
-          animation: "fade"
-    });
-	
-	$(function() {
-		$('.show_menu').click(function(){
-				$('.menu').fadeIn();
-				$('.show_menu').fadeOut();
-				$('.hide_menu').fadeIn();
-		});
-		$('.hide_menu').click(function(){
-				$('.menu').fadeOut();
-				$('.show_menu').fadeIn();
-				$('.hide_menu').fadeOut();
-		});
-	});
-  });
-</script>
-<!-----pop up
-<script type="text/javascript" src="js/jquery.reveal.js"></script>
-<link rel="stylesheet" href="css/reveal.css">
-<!-----pop up ned------>
-	
-<link rel="stylesheet" href="default.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="nivo-slider.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="slider.css" type="text/css" media="screen" />
-<script  type='text/javascript'>
-function validate(){
-	var uname=document.getElementById('name').value;
-	var emailid=document.getElementById('email').value;
-	var format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
-	var contact=document.getElementById('phone').value;
-	var catsel=document.getElementById('class');
-	var strcat = catsel.options[catsel.selectedIndex].value;
-	var upass=document.getElementById('pass').value;
-	if(uname==""){
-		alert("Please Enter a Name");
-		return false;
-	}
-	if(emailid==""){
-		alert("Please Enter Your emailaddress");
-		return false;
-	}
-	if(!emailid.match(format)){
-		alert("You have entered an wrong email address!"); 
-		return false;
-	}
-	if(contact==""){
-		alert("Please Enter Your contactno");
-		return false;
-	}
-	if(contact.length<10){
-		alert("Please Enter 10 digit phonenumber");
-		return false;
-	}
-	if(contact.length>10){
-		alert("Please Enter 10 digit phonenumber");
-		return false;
-	}
-	 if(strcat==0){
-		alert("Please Choose a Class");
-		return false;
-	}
-	if(upass==""){
-		alert("Please Enter Password");
-		return false;
-	}
-}
-function numberonly(){
-	var contact=document.getElementById('phone').value;
-	if(isNaN(contact)|| contact.indexOf(" ")!=-1){
-		alert("Enter numeric value");
-		return false;
-    }
-}
-</script>
-<script>
-<?php
-if($mess){
-?>
-alert('<?=$mess?>');
-<?php
-}
-?>
-</script>
-</head>
-<body style="background:#ccc; background:url(images/bg04.png) repeat #ccc;">
- <!--------------top bar -------->
- <div id="top_bar"  style="display: none;">
+
+ <!--div id="top_bar"  style="display: none;">
  		<div id="top_box">
 		
 		</div>
  </div>
- <!--------------top bar end-------->
-
- <!--------------header bar -------->
 <?php //include_once("topbar.php");?>
-<!----pop up------>
 <div id="myModal" class="reveal-modal" style="width:400px;">
 		<h1 style="font-size:20px;">Create Account</h1>
 		<form name="creataccount" action="creataction.php" method="post" onsubmit="return validate();">
@@ -148,7 +91,6 @@ alert('<?=$mess?>');
 						}
 						?>
 						</select>
-						<!--<input type="text" name="class" id="class"  class="form2"/>-->
 						</td>
 				</tr>
 				<tr>
@@ -165,17 +107,8 @@ alert('<?=$mess?>');
 		<a class="close-reveal-modal">&#215;</a>
 		</form>
 </div>
-<!----pop up end------>
-
- <!--------------header bar end -------->
-  
-  <!--------------menu bar -------->
 
    				<?php //include_once('menubar.php'); ?>
-
-<!--------------menu bar end -------->  
-  
-   <!--------------banner bar --------> 
  <div id="banner-bar" style="height:350px; margin-top:10px; display:none;">
  		<div id="banner1">
 				<div class="slider_container">
@@ -239,18 +172,12 @@ alert('<?=$mess?>');
    </div>
 </div>
  </div> 
-  <!--------------banner bar end-------->
-   
-<!--------------text line bar-------->
 <div id="textline_bar" style="display:none;">
 		<div id="textline_box">
 				<p class="text" style="color:#fff; margin-left:60px; margin-top:10px; float:left; font-style:italic; "><img src="image/img2.jpg" style="float:left; border-radius:70px;"/>
 				<span style="margin-top:25px; float:left; margin-left:10px;">This is demo text. To be modified with real content later.<br /> This is demo text. To be modified with real content later.This is demo text. To be modified with real content later.</span> </p>
 		</div>
 </div>
-<!--------------text line end-------->
-
-<!--------------hover box-------->
 <div id="hover_bar" style="display:none;">
 		<div id="hover_box">
 				<div id="hover_box2">
@@ -325,9 +252,6 @@ alert('<?=$mess?>');
 		</div>
 </div>
 </div>
-<!--------------hover box end-------->
- 
- <!--------------content---------> 
   <div id="content-menubar" style="display:none;">
 		<div id="content-menu">
 				<ul>
@@ -429,8 +353,7 @@ $(window).load(function() {
 				  </div>
 				</div>
 		</div>
- </div>
- <!--------------content bar emd---------> 
+ </div-->
  
  <!--------------footer---------> 
  <!--div id="footer-bar">
