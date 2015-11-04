12fe9696bcefec2432750d0ed6fba31da452cf74
anupsamantaray <anup.bapu@gmail.com>
2015-11-04 22:51:05 +0530

    commit

--- about_us.php
+++ about_us.php
@@ -1,6 +1,17 @@
 <?php include "common/header2.php"?>
 <div class="main_bg"><!-- start main -->
 	<div class="container">
+		<div class="about row">
+			<h2>about us</h2>
+			<p class="para">We are a dedicated team of professionals who want to make learning easy, simple and fun for students. Based out of Kolkata with Offices in Bangalore & Bhubaneswar we leverage technology to develop superior products & services that benefit students in an enlightening way. 
+			Our ScoreMore TM software runs on AI engine, and can identify weak areas of a student along. It then sends customised content to each student for better results well before the exams.</p>
+
+			<p class="para">Our Hall of Fame recognises students who not only top, but also show improvement in their scores. Students can  participate in online quiz and be winners.</p>
+
+			<p class="para">We have conducted such quizzes at many schools at Kolkata, Bhubaneswar and Raipur. Download our new App LearnQuix and carry the entire smart content with you. Students across India are already using our App for better performance.We also support GyanTarang, an organisation that supports a scholarship program for students.</p>
+			
+			<!--a href="single-page.php" class="fa-btn btn-1 btn-1e">read more</a-->
+		</div>
 		<div class="col-md-12 about row">
 			<div class="col-md-4 mapborder">
 				<img src="images/India-Map.jpg" alt="Lazy Owl Image" class="img-responsive">
