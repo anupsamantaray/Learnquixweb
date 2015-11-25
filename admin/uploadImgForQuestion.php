e6468d652ccc316e1c579f900144b1ded870b642
anupsamantaray <anup.bapu@gmail.com>
2015-11-25 18:16:12 +0530

    commit

--- admin/uploadImgForQuestion.php
+++ admin/uploadImgForQuestion.php
@@ -8,12 +8,12 @@ $img_type=$_FILES['image']['type'];
 //echo $img_type;
 if($img_type=='image/png' || $img_type=='image/jpeg' || $img_type=='image/gif' || $img_type=='image/bmp'){
 	$image=rand(0,999).time().$img_name;
-	$path="uploads/question_images/".$image;
+	$path="../courses/uploads/question_images/".$image;
 	$tmp_name=$_FILES['image']['tmp_name'];
 	if(move_uploaded_file($tmp_name,$path)){
 		 $pic=new Thumbnail();
-		 $pic->filename="uploads/question_images/".$image;
-		 $pic->filename2="uploads/question_images/question_images_thumb/thumb_".$image;
+		 $pic->filename="../courses/uploads/question_images/".$image;
+		 $pic->filename2="../courses/uploads/question_images/question_images_thumb/thumb_".$image;
 		 $pic->maxW=90;
 		 $pic->maxH=90;
 		 $pic->SetNewWH();
@@ -21,11 +21,11 @@ if($img_type=='image/png' || $img_type=='image/jpeg' || $img_type=='image/gif' |
 		 $pic->FinirPImage();
 		 $image = "thumb_".$image;
 		 
-		 $httphost = $_SERVER['HTTP_ORIGIN'].'/admin/';
+		 $httphost = $_SERVER['HTTP_ORIGIN'];
 		 if($_SERVER['HTTP_ORIGIN'] == 'http://localhost'){
-			 $httphost = $_SERVER['HTTP_ORIGIN'].'/Learnquixweb/admin/';
+			 $httphost = $_SERVER['HTTP_ORIGIN'].'/Learnquixweb/';
 		 }
-		 die(json_encode(array("status"=>"1","server"=>$httphost,"img_path"=>$httphost.$pic->filename2)));
+		 die(json_encode(array("status"=>"1","server"=>$httphost,"img_path"=>$httphost."courses/uploads/question_images/question_images_thumb/thumb_".$image)));
 	}
 }
 die(json_encode(array("status"=>"0")));
