e0c8a8ca8f3c97dad06b9af05a17322ad4ade401
Raju Rana <rana.raju1511@gmail.com>
2015-12-08 23:14:39 +0530

    commmit# Please enter the commit message for your changes. Lines starting

--- courses/view_concept_map_details.php
+++ courses/view_concept_map_details.php
@@ -9,10 +9,41 @@ if ($_SESSION['usrname']){
 if($_REQUEST['map_id']){
 	$id=$_GET['map_id'];
 }
+$randid=rand(1,10000000);
 ?>
 <script type="text/javascript">
-  $(function() {
+  $(function(){
 		$(".concept_mapcls").addClass("active");
+		setInterval(function(){showtime()},5000);
+		function showtime(){
+			var time=document.getElementById('txttimer').value;
+			var time=parseInt(time);
+			time=time+5;
+			document.getElementById('txttimer').value=time;
+			var rid=<?=$randid?>;
+			pth= document.getElementById("txtpth").value;
+			eml= document.getElementById("eml").value;
+			if (window.XMLHttpRequest) {
+				// code for IE7+, Firefox, Chrome, Opera, Safari
+				xmlhttp = new XMLHttpRequest();
+			} else {
+				// code for IE6, IE5
+				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xmlhttp.onreadystatechange = function() {
+				if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				   //alert(xmlhttp.responseText);
+					//document.getElementById("txttimer").innerHTML = xmlhttp.responseText;
+				}
+			}
+			xmlhttp.open("GET","get_time.php?q="+time+"&rid="+rid+"&pth="+pth+"&eml="+eml,true);
+			xmlhttp.send();
+			if(time==2700){
+				if (confirm("Do you want to appear for quiz ?") == true){
+					window.location.assign('show_quizzes_part_one.php');
+				} 
+			}
+		}
   });
 </script>
 	<div class="container">
@@ -29,6 +60,9 @@ if($_REQUEST['map_id']){
 							<img src="../admin/<?=$rslt['map_image']?>" style="height: 100%; width: 100%;">
 						</div>
 						<a href="javascript:void(0)" class="loginbutton" style="margin-top:15px;" onClick="history.back();">Back</a>
+						<input type='text' id='txttimer' value='0' style="display:none;"/>
+						<input type="text" id="txtpth" value="../admin/<?=$rslt['map_image']?>" style="display:none;"/>
+						<input type="text" id="eml" value="<?php echo $_SESSION['user_email']; ?>" style="display:none;"/>
 					</div>
 				<? } ?>
 				</div>
