488b31bf90e5b0a00fc634ae3ade0d197ebd97d7
unknown <arindam23>
2015-12-18 00:07:52 +0530

    commit

---
+++ site_map.php
@@ -0,0 +1,27 @@
+<?php include "common/header2.php"?>
+<div class="main_bg"><!-- start main -->
+	<div class="container">
+		<div class="about row">
+			<h2>Site Map</h2>
+			<div class="sitemap">
+				<ul>
+					<li>Your Text</li>
+					<li>Your Text</li>
+					<li>Your Text
+						<ul class="sub_sitemap">
+							<li>Your text</li>
+							<li>Your text</li>
+							<li>Your text</li>
+							<li>Your text</li>
+						</ul>
+					</li>
+					<li>Your Text</li>
+					<li>Your Text</li>
+				</ul>
+			</div>
+			
+			<!--a href="single-page.php" class="fa-btn btn-1 btn-1e">read more</a-->
+		</div>
+	</div>
+</div><!-- end main -->
+<?php include "common/footer.php"?>
