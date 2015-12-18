96d76d26aeeed0d39406aa693aa0172f2565a094
anupsamantaray <anup.bapu@gmail.com>
2015-12-19 00:14:07 +0530

    commit

--- courses/footer.php
+++ courses/footer.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row footer">
 			<div class="copy text-center">
-				<p class="link"><span>&#169; All rights reserved | <a href="#">Learn Quix</a></span> | <!--<span><a href="../technology.php">Technologies</a></span> | --><span><a href="../faq.php">FAQ</a></span>| <span><a href="../contact_us.php">Contact Us</a></span></p>
+				<p class="link"><span>&#169; All rights reserved | <a href="javascript:void(0)">Learn Quix</a></span> | <!--<span><a href="../technology.php">Technologies</a></span> | --><span><a href="../faq.php">FAQ</a></span>| <span><a href="../contact_us.php">Contact Us</a></span> | <span><a href="../site_map.php">Site Map</a></p>
 			</div>
 		</div>
 	</div>
--- site_map.php
+++ site_map.php
@@ -5,22 +5,32 @@
 			<h2>Site Map</h2>
 			<div class="sitemap">
 				<ul>
-					<li>Your Text</li>
-					<li>Your Text</li>
-					<li>Your Text
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about_us.php">About us</a></li>
+					<li><a href="courses/index.php">Courses</a></li>
+					<li>Quizzes
 						<ul class="sub_sitemap">
-							<li>Your text</li>
-							<li>Your text</li>
-							<li>Your text</li>
-							<li>Your text</li>
+							<li><a href="courses/index.php">Basic Quiz</a></li>
+							<li><a href="courses/quize_competitive.php">Compitative Quiz</a></li>
 						</ul>
 					</li>
-					<li>Your Text</li>
-					<li>Your Text</li>
+					<li><a href="hall_of_fame.php">Hall Of Frame</a></li>
+					<li><a href="games.php">Video & Fun Game</a></li>
+					<li>Remember Cards
+						<ul class="sub_sitemap">
+							<li><a href="pagetest.php">Memory Cards</a></li>
+							<li><a href="concept.php">Concept Map</a></li>
+							<li><a href="picture.php">Picture Game</a></li>
+						</ul>
+					</li>	
+					<li><a href="http://gyantarang.org/" target="_blank">Scolarship</a></li>
+					<li><a href="contact_us.php">Contact</a></li>
+					<li><a href="courses/ebook.php">Ebooks</a></li>
+					<li><a href="courses/video.php">Video</a></li>
+					<li><a href="courses/concept_map.php">Mind Map</a></li>
+					<li><a href="courses/sample_questions.php">Sample Question</a></li>
 				</ul>
-			</div>
-			
-			<!--a href="single-page.php" class="fa-btn btn-1 btn-1e">read more</a-->
+			</div>	
 		</div>
 	</div>
 </div><!-- end main -->
